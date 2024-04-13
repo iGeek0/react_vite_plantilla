b37130cf12f41364422ea5e11127a58904288688
iGeek <jesus.cardenas.d@gmail.com>
2024-04-13 09:39:43 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-primary text-center">
+        Plantila React + Vite + Bootstrap
+      </h1>
+    </>
+  );
+}
+
+export default App;
